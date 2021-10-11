da7961c72b32d751ef6f33b7c205cb0fd701248c
Nguyen Thanh Dat (APTECH HN) <datntth2010023@fpt.edu.vn>
2021-10-11 21:09:16 +0700

    aaaaa

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
 if($mysqli === false){
     die("ERROR: Could not connect. " .$mysqli->connect_error);
-}else{
+}/*else{
     echo "DB connection successful!";
-}
+}*/
 ?>
